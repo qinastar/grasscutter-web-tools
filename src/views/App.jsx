ce8337293b5b1cc99a3598d2f141df47bab7d9d3
lanrongqi <lancelrq@gmail.com>
2022-07-21 10:24:26 +0800

    init scripts

---
+++ src/views/App.jsx
@@ -0,0 +1,27 @@
+import { DatePicker } from 'antd';
+
+import logo from '@/images/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <DatePicker />
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
